6ab76937d2b9150f853e3aaf54fa8d68aae7c9fd
xegrassa <bacek90@mail.ru>
2020-04-17 16:57:58 +0600

    1

---
+++ task4/task4.py
@@ -0,0 +1,46 @@
+import sys
+from time import strptime
+
+
+def convert_to_minute(time):
+    hour, minute = map(int, time.split(':'))
+    return hour * 60 + minute
+
+
+def convert_to_hour_minute(time_minute):
+    hour = time_minute // 60
+    minute = time_minute % 60
+    if minute == 0:
+        minute = '00'
+    return f'{hour}:{minute}'
+
+
+def main():
+    times = []
+    with open(sys.argv[1], 'r', encoding='utf8') as file:
+        for visit in file:
+            in_time, out_time = map(convert_to_minute, visit.split())
+            times.append((in_time, 'start'))
+            times.append((out_time, 'end'))
+    times = sorted(times, key=lambda x: (x[0], len(x[1])))
+    visits = dict()
+    count, maxCount = 0, 0
+    for time in times:
+        if time[1] == 'start':
+            count += 1
+        else:
+            count -= 1
+        visits[time[0]] = count
+        maxCount = max(count, maxCount)
+    start_interval = False
+    for time, count_vis in sorted(visits.items()):
+        if count_vis == maxCount and not start_interval:
+            start_time = time
+            start_interval = True
+        elif count_vis != maxCount and start_interval:
+            print(*map(convert_to_hour_minute, (start_time, time)))
+            start_interval = False
+
+
+if __name__ == '__main__':
+    main()
